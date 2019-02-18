a5a6ade08ea0900910319578c1cdd39f8251abec
inigo.elorriaga <inigo.elorriaga@ekasa.net>
2019-02-18 14:24:57 +0100

    Cambios en game

---
+++ js/game.js
@@ -0,0 +1,40 @@
+var gun = Gun('https://webareaekasa.github.io');
+//var gun = Gun();
+//var points = {};
+
+  $(function() {
+
+    gun.get('mark').put({
+        name: "Mark",
+        email: "mark@gunDB.io",
+      });
+      
+      gun.get('mark').on(function(data, key){
+        console.log("update:", data);
+      });
+
+    // gun.get('points').on(function(data, key){
+    // console.log("update:", data);
+    // //points = data;
+    // });
+
+    // function addPoint(){
+    //     var newNumber = 0;
+    //     if(points.number){
+    //         newNumber = points.number + 1;
+    //     }
+        
+    //     var objToAdd = {
+    //         number: newNumber
+    //     };
+
+    //     gun.get('points').put(objToAdd);
+    // }
+
+    $( "#addBtn" ).on( "click", function() {
+        gun.get('mark').put({
+            name: "Pepe",
+            email: "mark@gunDB.io",
+          });
+    });
+});

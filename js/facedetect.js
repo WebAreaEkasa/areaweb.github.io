2e327ac9704b2c24178b4a60e5054ca5aeb00985
inigo.elorriaga <inigo.elorriaga@ekasa.net>
2019-02-21 09:28:47 +0100

    0.2 facedetect

--- js/facedetect.js
+++ js/facedetect.js
@@ -65,7 +65,7 @@ const labeledFaceDescriptors = await Promise.all(
 
 // 0.6 is a good distance threshold value to judge
 // whether the descriptors match or not
-const maxDescriptorDistance = 0.6
+const maxDescriptorDistance = 0.2
 const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, maxDescriptorDistance)
  //console.log("face matcher"+faceMatcher)
 const results = fullFaceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor))

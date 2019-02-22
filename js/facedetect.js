30ed5578421e4fab06b5d10654ec8f827effd68b
bitbaso <bitbaso@gmail.com>
2019-02-22 11:43:17 +0100

    facedetect ok

--- js/facedetect.js
+++ js/facedetect.js
@@ -1,24 +1,15 @@
+var labelImg;
 $(document).ready(function() {
-     
-  run1()
+  runOnLoad();
 })
 
-async function run1() {
-
-    const MODELS = "/data/weights/"; // Contains all the weights.
-
-    // await faceapi.loadSsdMobilenetv1Model(MODELS)
-    // await faceapi.loadFaceLandmarkModel(MODELS)
-    // await faceapi.loadFaceRecognitionModel(MODELS)
-      
-    await faceapi.loadTinyFaceDetectorModel(MODELS)
-    await faceapi.loadFaceLandmarkTinyModel(MODELS)
-    await faceapi.loadFaceRecognitionModel(MODELS)
-    
+async function runOnLoad() {
+  await loadModels();
+  labelImg = await Promise.all(await fecthImages());
 
 // try to access users webcam and stream the images
   // to the video element
- const videoEl = document.getElementById('player')
+ const videoEl = getPlayer();
   navigator.getUserMedia(
     { video: {} },
     stream => videoEl.srcObject = stream,
@@ -26,64 +17,117 @@ async function run1() {
 )
 }
 
-async function run2() {
+async function onPlay(videoEl) {
+  runOnPlay()
+  setTimeout(() => onPlay(videoEl), 1000)
+}
+
+async function runOnPlay() {
     
-//const mtcnnResults = await faceapi.ssdMobilenetv1(document.getElementById('player'))
-const mtcnnResults = await faceapi.tinyFaceDetector(document.getElementById('player'));
+  const mtcnnResults = await faceapi.ssdMobilenetv1(document.getElementById('player'))
+  //const mtcnnResults = await faceapi.tinyFaceDetector(document.getElementById('player'));
+
+  const input = getPlayer();
+  const overlay = getOverlay();
+  overlay.width = input.videoWidth;
+  overlay.height = input.videoHeight;
+  const detectionsForSize = mtcnnResults.map(det => det.forSize(500, 400))
+
+  //const tinyFaceDetectorOptions = getTinyFaceDetectorOptions();
+
+  //faceapi.drawDetection(overlay, detectionsForSize, { withScore: true })    
+
+  //const fullFaceDescriptions = await faceapi.detectAllFaces(input, tinyFaceDetectorOptions).withFaceLandmarks(true).withFaceDescriptors()
+  const fullFaceDescriptions = await faceapi.detectAllFaces(input).withFaceLandmarks().withFaceDescriptors()
+      
+  var labels = getPersonLabels();
+
+  const labeledFaceDescriptors = await Promise.all(
+    labels.map(async label => {
+
+      const img = getLabelImg(label);
+      
+      // detect the face with the highest score in the image and compute it's landmarks and face descriptor
+      //const fullFaceDescription = await faceapi.detectSingleFace(img, tinyFaceDetectorOptions).withFaceLandmarks(true).withFaceDescriptor()
+      const fullFaceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+      
+      if (!fullFaceDescription) {
+        throw new Error(`no faces detected for ${label}`)
+      }
+      
+      const faceDescriptors = [fullFaceDescription.descriptor]
+    // console.log(label)
+      return new faceapi.LabeledFaceDescriptors(label, faceDescriptors)
+    })
+  )
+
+  // 0.6 is a good distance threshold value to judge
+  // whether the descriptors match or not
+  const maxDescriptorDistance = 0.6;
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, maxDescriptorDistance)
+  //console.log("face matcher"+faceMatcher)
+  const results = fullFaceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor))
+
+  const boxesWithText = getBoxesWithText(fullFaceDescriptions, results);
+
+  faceapi.drawDetection(overlay, boxesWithText)
+}
 
 
-const input = document.getElementById('player');
-overlay.width = input.videoWidth;
-overlay.height = input.videoHeight;
-const detectionsForSize = mtcnnResults.map(det => det.forSize(500, 400))
+function getPlayer(){
+  return document.getElementById('player');
+}
 
-faceapi.drawDetection(overlay, detectionsForSize, { withScore: true })    
+function getOverlay(){
+  return document.getElementById('overlay');
+}
 
-const fullFaceDescriptions = await faceapi.detectAllFaces(input, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks(true).withFaceDescriptors()
+async function loadModels(){
+  const MODELS = "/data/weights/"; // Contains all the weights.
+
+  await faceapi.loadSsdMobilenetv1Model(MODELS)
+  await faceapi.loadFaceLandmarkModel(MODELS)
+  await faceapi.loadFaceRecognitionModel(MODELS)
     
-    
-const labels = ['elorriaga','crosetti']
+  // await faceapi.loadTinyFaceDetectorModel(MODELS)
+  // await faceapi.loadFaceLandmarkTinyModel(MODELS)
+  // await faceapi.loadFaceRecognitionModel(MODELS)
+}
 
-const labeledFaceDescriptors = await Promise.all(
-  labels.map(async label => {
+function getPersonLabels(){
+  return ['elorriaga','crosetti'];
+}
+
+function getTinyFaceDetectorOptions(){
+  return new faceapi.TinyFaceDetectorOptions();
+}
+
+function getBoxesWithText(fullFaceDescriptions, results){
+  return results.map((bestMatch, i) => {
+    const box = fullFaceDescriptions[i].detection.box
+    const text = bestMatch.toString()
+    const boxWithText = new faceapi.BoxWithText(box, text)
+    console.log(boxWithText);
+    return boxWithText;
+  })
+}
+
+async function fecthImages(){
+  var labels = getPersonLabels();
+  return labels.map(async label => {
     // fetch image data from urls and convert blob to HTMLImage element
     const imgUrl = `data/faces/${label}.png`
     const img = await faceapi.fetchImage(imgUrl)
-    
-    // detect the face with the highest score in the image and compute it's landmarks and face descriptor
-    const fullFaceDescription = await faceapi.detectSingleFace(img, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks(true).withFaceDescriptor()
-    
-    if (!fullFaceDescription) {
-      throw new Error(`no faces detected for ${label}`)
-    }
-    
-    const faceDescriptors = [fullFaceDescription.descriptor]
-   // console.log(label)
-    return new faceapi.LabeledFaceDescriptors(label, faceDescriptors)
-  })
-)
-
-// 0.6 is a good distance threshold value to judge
-// whether the descriptors match or not
-const maxDescriptorDistance = 0.2
-const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, maxDescriptorDistance)
- //console.log("face matcher"+faceMatcher)
-const results = fullFaceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor))
-
-
-const boxesWithText = results.map((bestMatch, i) => {
-  const box = fullFaceDescriptions[i].detection.box
-  const text = bestMatch.toString()
-  const boxWithText = new faceapi.BoxWithText(box, text)
-   return boxWithText;
-})
-
-faceapi.drawDetection(overlay, boxesWithText)
-
-
+    var labelImg = {label: label, img: img};
+    return labelImg;
+  });
 }
 
-async function onPlay(videoEl) {
-    run2()
-    setTimeout(() => onPlay(videoEl))
+
+function getLabelImg(label){
+  var outcome;
+  if(labelImg && label){
+    outcome = labelImg.filter(x => x.label == label)[0].img;
+  }
+  return outcome;
 }
